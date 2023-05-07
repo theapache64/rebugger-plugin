dccf9752fd35ddf34b549eb1b13a9000cbe1a07e
theapache64 <theapache64@gmail.com>
2023-05-07 16:46:30 +0530

    🚧 WIP : first good test

--- src/test/testData/basic/BasicAfter.kt
+++ src/test/testData/basic/BasicAfter.kt
@@ -11,10 +11,10 @@ fun MyFunction(
     param2 : Int
 ) {
     Rebugger(
-        trackMap = mapOf(
-            "param1" to param1,
-            "param2" to param2,
-        ),
+            trackMap = mapOf(
+                    "param1" to param1,
+                    "param2" to param2,
+            ),
     )
     Column {
         Text(text = "Param 1 is $param1 and Params 2 is $param2")
